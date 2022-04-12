ee103abae3fe4e1b05d8eae648a3249c4b8fc7a4
payalbanode0 <payalbanode0@gmail.com>
2022-04-12 21:12:30 +0530

    address book linq

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+
+namespace Day35AddressBookLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book Linq");
+            //UC 1 and 2 Create Address Book DataTable
+            DataTable addressBook = new DataTable();
+            addressBook.TableName = "AddressBook";
+            addressBook.Columns.Add("First Name");
+            addressBook.Columns.Add("Last Name");
+            addressBook.Columns.Add("Address");
+            addressBook.Columns.Add("City");
+            addressBook.Columns.Add("State");
+            addressBook.Columns.Add("Phone Number");
+            addressBook.Columns.Add("Email");
+
+        }
+    }
+}
