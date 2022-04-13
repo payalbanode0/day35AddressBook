c75bc473d16f48dd130ae962f4d614836cc33740
payalbanode0 <payalbanode0@gmail.com>
2022-04-13 13:02:42 +0530

    delete records

---
+++ day35AddressBookLinq/day35AddressBookLinq/AddressBookDataTablecs.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day35AddressBookLinq
+{
+    public class AddressBookDataTable
+    {
+        //UC 3 Insert New Contact to Address Book
+        public void AddToTable(DataTable addressBook)
+        {
+            addressBook.TableName = "AddressBook";
+            //Adding FirstName ,LastName,Address,City,State,PhoneNumber and Email.
+            addressBook.Rows.Add("Payal", "Banode", "25-4-71", "Nagpur", "Maharashtra", "8793819197", "payalabanode0@gmail.com");
+            addressBook.Rows.Add("Eren", "Jeager", "Shinsengumi", "Wall Maria", "Attak on Titan", "7958977310", "erenjeager@gmail.com");
+            addressBook.Rows.Add("Sasuke", "Uchiha", "4-3-333", "Leaf Village", "Naruto", "9106529025", "schihasasuke@gmail.com");
+            addressBook.Rows.Add("Kamado", "Tanjiro", "mt Kumotori", "Okutama", "Demon Slayer", "7578977310", "kamadoTanjiro@gmail.com");
+            Display(addressBook);
+        }
+
+        public void Display(DataTable addressBook)
+        {
+            foreach (DataRow row in addressBook.Rows)
+            {
+                string firstName = row["FirstName"].ToString();
+                string lastName = row["LastName"].ToString();
+                string address = row["Address"].ToString();
+                string city = row["City"].ToString();
+                string state = row["State"].ToString();
+                string phoneNumber = row["PhoneNumber"].ToString();
+                string email = row["Email"].ToString();
+                Console.WriteLine("\nFirst Name : " + firstName + ", Last Name : " + lastName + ", Address : " + address + ", City : " + city + ", State : " + state +
+                    ", Phone Number : " + phoneNumber + ", Email : " + email);
+            }
+        }
+        //UC 4 
+        public void EditRecord(DataTable addressBook)
+        {
+
+            Console.WriteLine("\nEnter a Name to Search");
+            string fname = Console.ReadLine();
+            foreach (DataRow row in addressBook.Rows)
+            {
+
+                if (Convert.ToString(row["FirstName"]) == fname)
+                {
+                    Console.WriteLine("Enter First Name to Replace all the row values");
+                    row["FirstName"] = Console.ReadLine();
+                    Console.WriteLine("Enter LastName");
+                    row["LastName"] = Console.ReadLine();
+                    Console.WriteLine("Enter Address");
+                    row["Address"] = Console.ReadLine();
+                    Console.WriteLine("Enter City");
+                    row["City"] = Console.ReadLine();
+                    Console.WriteLine("Enter State");
+                    row["State"] = Console.ReadLine();
+                    Console.WriteLine("Enter Phone Number");
+                    row["PhoneNumber"] = Console.ReadLine();
+                    Console.WriteLine("Enter Email");
+                    row["Email"] = Console.ReadLine();
+                    addressBook.AcceptChanges();
+                }
+            }
+            Console.WriteLine("\nDisplay AddressBook After Changes");
+
+            Display(addressBook);
+        }
+
+        //UC 5 Delete
+        public void DeleteRecords(DataTable addressBook)
+        {
+            Console.WriteLine("\nEnter a Name to Search");
+            string fname = Console.ReadLine();
+            for (int i = 0; i < addressBook.Rows.Count; i++)
+            {
+                DataRow dr = addressBook.Rows[i];
+                if (Convert.ToString(dr["FirstName"]) == fname)
+                {
+                    dr.Delete();
+                    addressBook.AcceptChanges();
+                }
+            }
+            Console.WriteLine("\nDisplay AddressBook After Changes");
+            Display(addressBook);
+        }
+    }
+}
---
+++ day35AddressBookLinq/day35AddressBookLinq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace Day35AddressBookLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book Linq");
+            AddressBookDataTable adb = new AddressBookDataTable();
+            //UC 1 and 2 Create Address Book DataTable
+            DataTable addressBook = new DataTable();
+            addressBook.TableName = "AddressBook";
+            addressBook.Columns.Add("FirstName");
+            addressBook.Columns.Add("LastName");
+            addressBook.Columns.Add("Address");
+            addressBook.Columns.Add("City");
+            addressBook.Columns.Add("State");
+            addressBook.Columns.Add("PhoneNumber");
+            addressBook.Columns.Add("Email");
+            Console.WriteLine("\nAddress Book Data Table Created");
+
+            //UC 3 Insert
+            adb.AddToTable(addressBook);
+
+            //UC 4 Edit
+            //adb.EditRecord(addressBook);
+
+            //UC 5 Delete
+            adb.DeleteRecords(addressBook);
+
+        }
+    }
+}
